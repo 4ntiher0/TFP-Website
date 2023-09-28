79c848011be2edf805c4858e1339064385bde001
4ntiher0 <110702556+4ntiher0@users.noreply.github.com>
2023-09-28 08:30:20 -0500

    css toggle revision to colors

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById('toggleButton').addEventListener('click', function() {
     var link = document.getElementById('cssLink');
-    if (link.getAttribute('href') === 'style.css') {
+    if (link.getAttribute('href') === 'stylelt.css') {
         link.setAttribute('href', 'styles.css');
     } else {
-        link.setAttribute('href', 'style.css');
+        link.setAttribute('href', 'stylelt.css');
     }
 });
