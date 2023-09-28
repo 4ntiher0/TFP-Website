51e66d9d8cfbc80f110916db5b761af29adfa530
4ntiher0 <110702556+4ntiher0@users.noreply.github.com>
2023-09-28 15:23:25 -0500

    updates

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('toggleButton').addEventListener('click', function() {
-    var link = document.getElementById('cssLink');
-    if (link.getAttribute('href') === 'stylelt.css') {
-        link.setAttribute('href', 'styles.css');
-    } else {
-        link.setAttribute('href', 'stylelt.css');
-    }
-});
